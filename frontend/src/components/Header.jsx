a3126522ce5e0a95d6d4c27daabd86b75a746bf9
Kolilia <jwm20002@mail.ru>
2021-09-28 20:12:44 +0400

    ffff

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,27 @@
+import { makeStyles } from "@material-ui/core/styles";
 import { Close, Menu } from "@material-ui/icons";
 import React, { useState } from "react";
 import ZeusButton from "./System/ZeusButton";
 
+const useStyles = makeStyles({
+  icon: {
+    marginRight: ".1rem",
+  },
+});
+
 const Header = () => {
+  const classes = useStyles();
   const [open, setOpen] = useState(false);
 
   return (
     <header>
       <div id="main-content">
         <ZeusButton onClick={open ? () => setOpen(false) : () => setOpen(true)}>
-          {open ? <Close /> : <Menu />}
+          {open ? (
+            <Close className={classes.icon} />
+          ) : (
+            <Menu className={classes.icon} />
+          )}
           Каталог
         </ZeusButton>
       </div>
