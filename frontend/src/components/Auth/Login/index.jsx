06cc5878a63aefa289f991dfc3f06838e18c048d
Kolymaznov Ilya <jwm20002@mail.ru>
2021-10-07 14:43:36 +0400

    fff

--- frontend/src/components/Auth/Login/index.jsx
+++ frontend/src/components/Auth/Login/index.jsx
@@ -1,6 +1,8 @@
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { secondaryThemeColor } from "../../../helpers/colors";
+import ZeusButton from "./../../System/ZeusButton";
 
 const useStyles = makeStyles({
   root: {
@@ -18,17 +20,45 @@ const useStyles = makeStyles({
     maxWidth: 500,
     width: "100%",
   },
+  actions: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  link: {
+    color: secondaryThemeColor,
+    marginTop: ".6rem",
+  },
 });
 
 const Login = () => {
   const location = useLocation();
   const classes = useStyles();
 
-  console.log(location.state);
-
   return (
     <div className={classes.root}>
-      <div className={classes.block}></div>
+      <h1>Авторизация</h1>
+      <div className={classes.block}>
+        {location?.state?.from && (
+          <p
+            style={{
+              color: secondaryThemeColor,
+              fontSize: ".85rem",
+              textAlign: "center",
+              fontWeight: 500,
+              margin: 0,
+              marginBottom: "1.5rem",
+            }}
+          >
+            Чтобы пройти дальше вам, необходимо авторизоваться
+          </p>
+        )}
+
+        <div className={classes.actions}>
+          <ZeusButton>Войти</ZeusButton>
+          <Link className={classes.link}>Нет аккаунта? Зарегистрироваться</Link>
+        </div>
+      </div>
     </div>
   );
 };
