29409b4a81ca5107e2c45f05c8bd40b27814cae5
MindTerror <qilave@mail.ru>
2022-10-03 22:07:52 +0300

    api url change

--- super-front/src/endpoints.js
+++ super-front/src/endpoints.js
@@ -1,27 +1,27 @@
-const url_backend = "http://localhost:3000/api";
+const url_backend = "https://shop-zeus-back.vercel.app/api";
 
 export const endpoints = {
-  auth: {
-    login: `${url_backend}/v1/auth/login`,
-    register: `${url_backend}/v1/auth/register`,
-    fetchProfile: `${url_backend}/v1/auth/profile`,
-  },
-  admin: {
-    fetchClients: `${url_backend}/v1/admin/users`,
-  },
-  products: {
-    createProduct: `${url_backend}/v1/admin/products`,
-    fetchProductsByAdmin: `${url_backend}/v1/admin/products`,
-    fetchProduct: `${url_backend}/v1/products/{productId}`,
-    changeProductByAdmin: `${url_backend}/v1/admin/products/{productId}`,
-    replaceImageByAdmin: `${url_backend}/v1/admin/products/replace-image/{productId}`,
-    fetchProducts: `${url_backend}/v1/products`,
-  },
-  orders: {
-    fetchOrders: `${url_backend}/v1/orders`,
-    createOrder: `${url_backend}/v1/orders`,
-    completeOrder: `${url_backend}/v1/admin/orders/{orderId}`,
-    fetchOrderByAdmin: `${url_backend}/v1/admin/orders`,
-    fetchOrderByAdminCsv: `${url_backend}/v1/admin/orders/csv`,
-  },
+    auth: {
+        login: `${url_backend}/v1/auth/login`,
+        register: `${url_backend}/v1/auth/register`,
+        fetchProfile: `${url_backend}/v1/auth/profile`,
+    },
+    admin: {
+        fetchClients: `${url_backend}/v1/admin/users`,
+    },
+    products: {
+        createProduct: `${url_backend}/v1/admin/products`,
+        fetchProductsByAdmin: `${url_backend}/v1/admin/products`,
+        fetchProduct: `${url_backend}/v1/products/{productId}`,
+        changeProductByAdmin: `${url_backend}/v1/admin/products/{productId}`,
+        replaceImageByAdmin: `${url_backend}/v1/admin/products/replace-image/{productId}`,
+        fetchProducts: `${url_backend}/v1/products`,
+    },
+    orders: {
+        fetchOrders: `${url_backend}/v1/orders`,
+        createOrder: `${url_backend}/v1/orders`,
+        completeOrder: `${url_backend}/v1/admin/orders/{orderId}`,
+        fetchOrderByAdmin: `${url_backend}/v1/admin/orders`,
+        fetchOrderByAdminCsv: `${url_backend}/v1/admin/orders/csv`,
+    },
 };
