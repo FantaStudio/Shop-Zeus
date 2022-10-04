3f3fb715c91e5460db0c0aea6067d258e8115651
MindTerror <qilave@mail.ru>
2022-10-04 14:46:21 +0300

    static for vercel

--- backend/index.js
+++ backend/index.js
@@ -15,16 +15,16 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.append("Access-Control-Allow-Origin", ["*"]);
-  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE, PATCH");
-  res.append(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Accept"
-  );
-  next();
+    res.append("Access-Control-Allow-Origin", ["*"]);
+    res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE, PATCH");
+    res.append(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, Accept"
+    );
+    next();
 });
 
-app.use("/static", express.static(path.join(__dirname, "public")));
+app.use("/static", express.static("public"));
 
 app.use(productsRoutes);
 app.use(authRoutes);
@@ -32,18 +32,18 @@ app.use(adminRoutes);
 app.use(ordersRoutes);
 
 const start = async () => {
-  try {
-    await mongoose.connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
 
-    app.listen(PORT, () => {
-      console.log(`Server has been started`);
-    });
-  } catch (err) {
-    console.log(err);
-  }
+        app.listen(PORT, () => {
+            console.log(`Server has been started`);
+        });
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 start();
